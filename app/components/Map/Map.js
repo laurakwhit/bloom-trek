be96abcd47c7c19656f5db625d436b5b5463b92d
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-10-27 11:11:47 -0600

    Configure eslint and add proptypes

--- app/components/Map/Map.js
+++ app/components/Map/Map.js
@@ -1,60 +1,76 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
 import { MapView } from 'expo';
 
 const deltas = {
-	latitudeDelta: 1,
-	longitudeDelta: 1
+  latitudeDelta: 1,
+  longitudeDelta: 1,
 };
 
 const initialRegion = {
-	latitude: 39.739,
+  latitude: 39.739,
   longitude: -104.99,
 };
 
-const Marker = MapView.Marker;
+const { Marker } = MapView;
 
 export default class Map extends Component {
+  renderMarkers = () => {
+    const { parks, handleSelectedPark } = this.props;
 
-	renderMarkers = () => {
-		return this.props.parks.map((park, i) => (
-			<Marker key={i} title={park.name} coordinate={park.coords} onPress={() => this.props.handleSelectedPark(park.id)}/>
-		));
-	}
+    parks.map(park => (
+      <Marker
+        key={park.id}
+        title={park.name}
+        coordinate={park.coords}
+        onPress={() => handleSelectedPark(park.id)}
+      />
+    ));
+  };
 
-	render() {
-		const region = {
-			...this.props.location,
-      ...deltas
-		};
+  render() {
+    const { location } = this.props;
+    const region = {
+      ...location,
+      ...deltas,
+    };
 
-		if (!region.latitude || !region.longitude) {
-			return (
-				<View style={styles.container}>
-					<Text>Loading map...</Text>
-				</View>
-			);
-		}
+    if (!region.latitude || !region.longitude) {
+      return (
+        <View style={styles.container}>
+          <Text>Loading map...</Text>
+        </View>
+      );
+    }
 
-		return (
-			<MapView
-				style={styles.container}
-				region={region}
-				initialRegion={{ ...initialRegion, ...deltas }}
+    return (
+      <MapView
+        style={styles.container}
+        region={region}
+        initialRegion={{ ...initialRegion, ...deltas }}
         showsUserLocation
         showsMyLocationButton
-			>
-				{this.renderMarkers()}
-			</MapView>
-		);
-	}
+      >
+        {this.renderMarkers()}
+      </MapView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		width: '100%',
+  container: {
+    width: '100%',
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-	}
+  },
 });
+
+Map.propTypes = {
+  parks: PropTypes.arrayOf(PropTypes.objects).isRequired,
+  handleSelectedPark: PropTypes.func.isRequired,
+  location: PropTypes.objectOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.num]),
+  ).isRequired,
+};
--- app/screens/HomeScreen/HomeScreen.js
+++ app/screens/HomeScreen/HomeScreen.js
@@ -1,67 +1,72 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
-import Map from '../../components/Map/Map';
 import { Location, Permissions } from 'expo';
+import Map from '../../components/Map/Map';
 import { getAllParks } from '../../utils/api';
 
 export default class HomeScreen extends Component {
-	state = {
-		location: null,
-		errorMessage: null,
-		parks: [],
-		selectedPark: null,
-	};
+  state = {
+    location: null,
+    errorMessage: null,
+    parks: [],
+    selectedPark: null,
+  };
 
-	componentDidMount() {
-		this.getCurrentLocation()
-		this.getParks()
+  componentDidMount() {
+    this.getCurrentLocation();
+    this.getParks();
   }
 
-	getCurrentLocation = async () => {
-		const { status } = await Permissions.askAsync(Permissions.LOCATION);
+  getCurrentLocation = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
       this.setState({
         errorMessage: 'Permission to access location was denied...',
       });
     }
 
-		const position = await Location.getCurrentPositionAsync({});
-		const location = {
-			latitude: position.coords.latitude,
-			longitude: position.coords.longitude,
-		};
-    this.setState({ location });	
-	};
+    const position = await Location.getCurrentPositionAsync({});
+    const location = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    };
+    this.setState({ location });
+  };
 
-	getParks = async () => {
-		const unformattedParks = await getAllParks();
-		const parks = unformattedParks.map(park => ({
-			id: park.id,
-			name: park.name,
-			coords: {
-				latitude: park.latitude,
-				longitude: park.longitude,
-			},
-		}));
-		this.setState({ parks });
-	}
+  getParks = async () => {
+    const unformattedParks = await getAllParks();
+    const parks = unformattedParks.map(park => ({
+      id: park.id,
+      name: park.name,
+      coords: {
+        latitude: park.latitude,
+        longitude: park.longitude,
+      },
+    }));
+    this.setState({ parks });
+  };
 
-	handleSelectedPark = (id) => {
-		this.setState({ selectedPark: id });
-		console.log(id)
-	}
+  handleSelectedPark = (id) => {
+    this.setState({ selectedPark: id });
+  };
 
-	render() {
-		const { location, parks } = this.state;
+  render() {
+    const { location, parks } = this.state;
 
-		return (
-			<View style={{ flex: 7 }}>
-				<Map location={location} parks={parks} handleSelectedPark={this.handleSelectedPark}/>
-			</View>
-		);
-	}
+    return (
+      <View style={styles.container}>
+        <Map
+          location={location}
+          parks={parks}
+          handleSelectedPark={this.handleSelectedPark}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = {
-
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 7,
+  },
+});
