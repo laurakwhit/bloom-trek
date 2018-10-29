be5b8592f1bfdd602d658335fcb9b2cb76a51360
Chris Boylen <billpolster@gmail.com>
2018-10-29 14:07:35 -0600

    Delete google variable

--- app/__tests__/HomeScreen-test.js
+++ app/__tests__/HomeScreen-test.js
@@ -2,45 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 
-const setupGoogleMock = () => {
-  const google = {
-    maps: {
-      places: {
-        AutocompleteService: () => {},
-        PlacesServiceStatus: {
-          INVALID_REQUEST: 'INVALID_REQUEST',
-          NOT_FOUND: 'NOT_FOUND',
-          OK: 'OK',
-          OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
-          REQUEST_DENIED: 'REQUEST_DENIED',
-          UNKNOWN_ERROR: 'UNKNOWN_ERROR',
-          ZERO_RESULTS: 'ZERO_RESULTS',
-        },
-      },
-      Geocoder: () => {},
-      GeocoderStatus: {
-        ERROR: 'ERROR',
-        INVALID_REQUEST: 'INVALID_REQUEST',
-        OK: 'OK',
-        OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
-        REQUEST_DENIED: 'REQUEST_DENIED',
-        UNKNOWN_ERROR: 'UNKNOWN_ERROR',
-        ZERO_RESULTS: 'ZERO_RESULTS',
-      },
-    },
-  };
-  global.window.google = google;
-};
-
 describe('HomeScreen', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(<HomeScreen />);
-  });
-
-  beforeAll(() => {
-    setupGoogleMock();
   });
 
   it('should match snapshot', () => {
