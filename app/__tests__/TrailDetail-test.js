d9c0ec06b6601741b49d69eb10be54f1359cd602
Chris Boylen <billpolster@gmail.com>
2018-10-30 16:58:29 -0600

    Add and pass test TrailDetail matches snapshot

---
+++ app/__tests__/TrailDetail-test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TrailDetail from '../components/TrailDetail/TrailDetail';
+
+describe('TrailDetail', () => {
+  let mockResetSelectedTrail;
+  let mockSelectedTrail;
+  let wrapper;
+
+  beforeEach(() => {
+    mockResetSelectedTrail = jest.fn();
+    mockSelectedTrail = {
+      id: 1,
+      name: 'Castlewood Canyon - Rimrock to Creek Bottom Loop',
+      uid: 7006769,
+      difficulty: 'medium',
+      length: 4.6,
+      status: 'All Clear',
+      trail_img_url: 'https://cdn-files.apstatic.com/hike/7006325_medium_1438452681.jpg',
+      trail_url: 'https://www.hikingproject.com/trail/7006769/castlewood-canyon-rimrock-to-creek-bottom-loop',
+      summary: 'A fun, rolling trail with a sense of seclusion, great geology, and historical sites.',
+      park_id: 1,
+      coords: {
+        latitude: 39.3598,
+        longitude: -104.7682,
+      },
+    };
+    wrapper = shallow(
+      <TrailDetail
+        selectedTrail={mockSelectedTrail}
+        resetSelectedTrail={mockResetSelectedTrail}
+      />,
+    );
+  });
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
