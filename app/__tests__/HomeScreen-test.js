b593709bf14bf5fbf47d6c9d86e1fd9f2a1b356f
Chris Boylen <billpolster@gmail.com>
2018-10-31 15:13:05 -0600

    Add and pass test hadleSelectedPark sets state

--- app/__tests__/HomeScreen-test.js
+++ app/__tests__/HomeScreen-test.js
@@ -3,9 +3,21 @@ import { shallow } from 'enzyme';
 import HomeScreen from '../screens/HomeScreen';
 
 describe('HomeScreen', () => {
+  let unformattedParks;
   let wrapper;
 
   beforeEach(() => {
+    unformattedParks = [{
+      id: 1,
+      name: 'Castlewood Canyon',
+      coords: {
+        latitude: 39.3379,
+        longitude: -104.7512,
+      },
+    }];
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(unformattedParks),
+    }));
     wrapper = shallow(<HomeScreen />);
   });
 
@@ -15,22 +27,20 @@ describe('HomeScreen', () => {
 
   describe('getParks', () => {
     it('should set state with parks', async () => {
-      const unformattedParks = [{
-        id: 1,
-        name: 'Castlewood Canyon',
-        coords: {
-          latitude: 39.3379,
-          longitude: -104.7512,
-        },
-      }];
-
-      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-        json: () => Promise.resolve(unformattedParks),
-      }));
-
       await wrapper.instance().getParks();
 
       expect(wrapper.state().parks).toEqual(unformattedParks);
+    });
+  });
+
+  describe('handleSelectedPark', () => {
+    it('should set state when invoked ', async () => {
+      wrapper.setState({ parks: unformattedParks });
+      await wrapper.instance().handleSelectedPark(1);
+
+      expect(wrapper.state().selectedPark).toEqual(1);
+      expect(wrapper.state().selectedTrail).toEqual(null);
+      expect(wrapper.state().trails).toEqual(unformattedParks);
     });
   });
 });
