db2654f5852a1eccdd07c5255858a44980c74e1f
Chris Boylen <billpolster@gmail.com>
2018-10-31 08:35:33 -0600

    Add and pass test getParkTrails should be invoked with correct params

--- app/__tests__/api-test.js
+++ app/__tests__/api-test.js
@@ -32,5 +32,38 @@ describe('API', () => {
 
       expect(result).toEqual(mockPark);
     });
+
+  describe('getParkTrails', () => {
+    let mockTrail;
+
+    beforeEach(() => {
+      mockTrail = [{
+        id: 1,
+        name: 'Castlewood Canyon - Rimrock to Creek Bottom Loop',
+        uid: 7006769,
+        difficulty: 'medium',
+        length: 4.6,
+        status: 'All Clear',
+        trail_img_url: 'https://cdn-files.apstatic.com/hike/7006325_medium_1438452681.jpg',
+        trail_url: 'https://www.hikingproject.com/trail/7006769/castlewood-canyon-rimrock-to-creek-bottom-loop',
+        summary: 'A fun, rolling trail with a sense of seclusion, great geology, and historical sites.',
+        park_id: 1,
+        coords: {
+          latitude: 39.3598,
+          longitude: -104.7682,
+        },
+      }];
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        json: () => Promise.resolve(mockTrail),
+      }));
+    });
+
+    it('should be invoked with correct params', async () => {
+      const id = 1;
+      const url = `https://bloom-trek-api.herokuapp.com/api/v1/parks/${id}/trails`;
+      await getParkTrails(id);
+
+      expect(window.fetch).toHaveBeenCalledWith(url);
+    });
   });
 });
