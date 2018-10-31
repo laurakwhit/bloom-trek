09428d6f97a8bdd68b409696926f4bc67bbf4081
Chris Boylen <billpolster@gmail.com>
2018-10-30 20:24:53 -0600

    Add and pass test onSwipeRight should invoke right swipe

--- app/__tests__/FlowerDetail-test.js
+++ app/__tests__/FlowerDetail-test.js
@@ -38,5 +38,14 @@ describe('FlowerDetail', () => {
 
       expect(mockResetSelectedFlower).toHaveBeenCalled();
     });
+
+    it('should invoke onSwipeRight on right swipe', () => {
+      const spy = spyOn(wrapper.instance(), 'onSwipeRight');
+      wrapper.instance().forceUpdate();
+
+      wrapper.find('GestureRecognizer').props().onSwipeRight();
+
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
